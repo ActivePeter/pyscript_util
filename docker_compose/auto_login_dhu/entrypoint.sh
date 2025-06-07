bf8cb8493b9d533302775cebb307f9b36001223b
ActivePeter <1020401660@qq.com>
2025-06-07 03:23:43 -0700

    basic pyscript_util

--- docker_compose/auto_login_dhu/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-echo "Auto Login Entrypoint"
-
-# chromedriver
-
-google-chrome --no-sandbox
-
-python3 /auto_login/script.py
--- git_config_usr_name.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name ActivePeter
-git config --global user.email 1020401660@qq.com
